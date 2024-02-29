ed668c4de20f35545fdc0145b41fd306ca915ff8
Matei-Alexandru Ciucă <86956363+matei1903@users.noreply.github.com>
2024-02-29 11:31:46 +0200

    bg_color_verde

--- apps/client/App.jsx
+++ apps/client/App.jsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
 ]);
 const GlobalStypes = createGlobalStyle`
   body {
-    background: #542537;
+    background: #202b1b;
     color: ${variables.primaryColor};
     font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
