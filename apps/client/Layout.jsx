1d3cac6ba759c17eab3bc438ac84f056ccfb9bde
Matei-Alexandru Ciucă <86956363+matei1903@users.noreply.github.com>
2024-03-01 17:48:21 +0200

    test_logo

--- apps/client/Layout.jsx
+++ apps/client/Layout.jsx
@@ -15,7 +15,7 @@ export default ({ children }) => {
     return (
         <StyledLayout>
             <div className="header">
-              <img src="gs://quickbite-844b3.appspot.com/logo_quickbite.png"></img>
+              <img src="https://firebasestorage.googleapis.com/v0/b/quickbite-844b3.appspot.com/o/logo_quickbite.png?alt=media&token=356b8ce3-e2e0-4584-a46a-656992a181f3"></img>
             </div>
             <div>{children}</div>
         </StyledLayout>
