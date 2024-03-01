f812c880f84b4ab009be640c7b898ee0a002d231
Matei-Alexandru Ciucă <86956363+matei1903@users.noreply.github.com>
2024-03-01 16:48:58 +0200

    logo#2

--- apps/client/Layout.jsx
+++ apps/client/Layout.jsx
@@ -15,7 +15,7 @@ export default ({ children }) => {
     return (
         <StyledLayout>
             <div className="header">
-              <img src="gs://quickbite-844b3.appspot.com/logo_quickbite.png"></img>
+              <img src="gs://quickbite-844b3.appspot.com/logo_quickbite.png" alt="logo_quickbite"></img>
             </div>
             <div>{children}</div>
         </StyledLayout>
