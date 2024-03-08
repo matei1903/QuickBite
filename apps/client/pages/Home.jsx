6ba5646a9f145d930922a482295c885d319ebd15
Matei-Alexandru Ciucă <86956363+matei1903@users.noreply.github.com>
2024-03-08 19:22:07 +0200

    repair4

--- apps/client/pages/Home.jsx
+++ apps/client/pages/Home.jsx
@@ -129,19 +129,19 @@ export default () => {
     const garnituriQuery = query(collection(db, "/garnituri"));
     const desertQuery = query(collection(db, "/desert"));
     const bauturiQuery = query(collection(db, "/bauturi"));
-  
+
     const combinedQuery = query(aperitiveQuery, felPrincipalQuery, supeCiorbeQuery, salateQuery, pasteQuery, pizzaQuery, garnituriQuery, desertQuery, bauturiQuery);
-  
-    const unsubscribe = onSnapshot(combinedQuery, (snapshot) => {
+
+    onSnapshot(combinedQuery, (snapshot) => {
       const _docs = [];
-  
+
       snapshot.forEach((doc) => {
         _docs.push({ ...doc.data(), id: doc.id });
       });
-  
       setDocs(_docs);
     });
   }, []);
+  
   return (
     <Layout>
       <StyledHome>
