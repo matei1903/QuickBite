67630d4507f3857bf4ad68ac6943299472a11a34
Matei-Alexandru Ciucă <86956363+matei1903@users.noreply.github.com>
2024-03-08 19:12:10 +0200

    repair3

--- apps/client/pages/Home.jsx
+++ apps/client/pages/Home.jsx
@@ -4,8 +4,7 @@ const Layout = React.lazy(() => import("../Layout.jsx"));
 const Icon = React.lazy(() =>
   import("@quick-bite/components/common/Icon.jsx")
 );
-import { collection, onSnapshot, query, getFirestore } from "firebase/firestore";
-const db = getFirestore();
+import { collection, onSnapshot, query } from "firebase/firestore";
 import { useFirebase } from "@quick-bite/components/context/Firebase";
 const StyledHome = styled.div`
   h1 {
@@ -142,9 +141,6 @@ export default () => {
   
       setDocs(_docs);
     });
-  
-    // Returnează funcția de curățare pentru dezabonare
-    return () => unsubscribe();
   }, []);
   return (
     <Layout>
