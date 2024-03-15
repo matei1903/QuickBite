46ca102b355bed48458cbeb7c6dc1a717f49fd50
Matei-Alexandru Ciucă <86956363+matei1903@users.noreply.github.com>
2024-03-15 18:17:24 +0200

    buton_profil

--- apps/client/Layout.jsx
+++ apps/client/Layout.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import React, { useState, useEffect } from "react";
+import guestIcon from 'https://firebasestorage.googleapis.com/v0/b/quickbite-844b3.appspot.com/o/user.png?alt=media&token=364d98fb-2720-4fa6-b1e9-2ea9b538280b';
 import { useFirebase } from "@quick-bite/components/context/Firebase";
 const StyledLayout = styled.div`
   .header {
@@ -17,16 +19,64 @@ const StyledLayout = styled.div`
     top: 10px;
   }
 `;
+const RoundButton = styled.button`
+  .profile_button {
+    padding: 0;
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    margin-top: -6px;
+    float: right;
+    position: relative;
+    z-index: 4;
+   }
+`;
+const ProfileImage = styled.img`
+  /* Stiluri pentru imaginea de profil */
+`;
+
+
 export default ({ children }) => {
     const _firebase = useFirebase();
     const db = _firebase?.db;
+    const _firebase = useFirebase();
+    const db = _firebase?.db;
 
+    // Starea pentru imaginea utilizatorului și conectare
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [userImage, setUserImage] = useState(null);
+
+    // Funcție pentru încărcarea imaginii utilizatorului
+    const loadUserImage = () => {
+        // Verificați dacă utilizatorul este autentificat și încărcați imaginea corespunzătoare
+        // Înlocuiți logica următoare cu încărcarea imaginii utilizatorului din baza de date sau de la serviciul de autentificare
+        const user = _firebase?.auth?.currentUser;
+        if (user && user.photoURL) {
+            setUserImage(user.photoURL);
+        } else {
+            setUserImage(guestIcon);
+        }
+    };
+
+    // Funcție pentru gestionarea clicului butonului
+    const handleButtonClick = () => {
+        // Implementați aici acțiunea butonului
+        // De exemplu, puteți deschide o fereastră modală pentru profilul utilizatorului sau pentru autentificare
+    };
+
+    // Efect secundar pentru încărcarea imaginii utilizatorului atunci când este conectat
+    useEffect(() => {
+        loadUserImage();
+    }, [isLoggedIn]);
     if (!db)
         return <div>loading...</div>
     return (
         <StyledLayout>
             <div className="header">
               <img src="https://firebasestorage.googleapis.com/v0/b/quickbite-844b3.appspot.com/o/logo_quickbite.png?alt=media&token=356b8ce3-e2e0-4584-a46a-656992a181f3" className="img"></img>
+              <RoundButton className="profile_button" onClick={handleButtonClick}>
+                    <ProfileImage src={userImage} alt={isLoggedIn ? "Profile" : "Guest"} />
+              </RoundButton>
             </div>
             <div>{children}</div>
         </StyledLayout>
