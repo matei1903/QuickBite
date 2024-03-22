e9cb606e39a6110cc5aa2ab4898359b3ed6834f0
Matei-Alexandru Ciucă <86956363+matei1903@users.noreply.github.com>
2024-03-22 18:17:59 +0200

    buton_profil

--- apps/client/Layout.jsx
+++ apps/client/Layout.jsx
@@ -37,6 +37,9 @@ const RoundButton = styled.button`
     float: right;
     position: relative;
     z-index: 998;
+    background-image: url('https://firebasestorage.googleapis.com/v0/b/quickbite-844b3.appspot.com/o/user.png?alt=media&token=364d98fb-2720-4fa6-b1e9-2ea9b538280b');
+    background-size: cover;
+    background-position: center;
    }
 `;
 const ProfileImage = styled.img`
@@ -82,7 +85,6 @@ export default ({ children }) => {
             <div className="header">
               <img src="https://firebasestorage.googleapis.com/v0/b/quickbite-844b3.appspot.com/o/logo_quickbite.png?alt=media&token=356b8ce3-e2e0-4584-a46a-656992a181f3" className="img"></img>
               <RoundButton className="profile_button" onClick={handleButtonClick}>
-                    <img className="img_profil" src="https://firebasestorage.googleapis.com/v0/b/quickbite-844b3.appspot.com/o/user.png?alt=media&token=364d98fb-2720-4fa6-b1e9-2ea9b538280b"></img>
                     <ProfileImage src={userImage} alt={isLoggedIn ? "Profile" : "Guest"} />
               </RoundButton>
             </div>
