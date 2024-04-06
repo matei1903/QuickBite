8bb8ffd29f494b61755ca9f4e42c31481b5916ec
Matei-Alexandru Ciucă <86956363+matei1903@users.noreply.github.com>
2024-04-06 20:26:00 +0300

    fix_vite

--- apps/client/vite.config.js
+++ apps/client/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import reactFire from "@react-firebase/reactfire";
 
 export default defineConfig({
   plugins: [react()],
