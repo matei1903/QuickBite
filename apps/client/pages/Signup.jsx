3069b7239e6d5d0bed8f2de3707ce3605cf85c7b
Matei-Alexandru Ciucă <86956363+matei1903@users.noreply.github.com>
2024-04-12 03:09:36 +0300

    new_style5

--- apps/client/pages/Signup.jsx
+++ apps/client/pages/Signup.jsx
@@ -26,7 +26,7 @@ const Input = styled.input`
   border: 0;
   font: inherit;
   padding: 0.5rem;
-  width: calc(100% - 2rem);
+  width: 100%
 
   &::placeholder {
     color: #7e8ba3;
@@ -47,6 +47,7 @@ const SubmitButton = styled.input`
   color: #fff;
   margin-bottom: 6rem;
   width: 100%;
+  font: 20px;
 `;
 
 const Signup = () => {
@@ -60,13 +61,17 @@ const Signup = () => {
 
   const handleSignup = async (e) => {
     e.preventDefault();
-    if (password !== conf_pass)
-      alert("Parola confirmata este diferita!");
+
     try {
       const authResult = await createUserWithEmailAndPassword(auth, email, password);
       await addUserInfoToFirestore(authResult.user.uid, email, authResult.user.photoURL);
-      alert("Contul a fost creat cu succes!");
-      navigate('/home');
+      if (password !== conf_pass)
+        alert("Parola confirmata este diferita!");
+      else {
+        alert("Contul a fost creat cu succes!");
+        navigate('/home');
+      }
+
     } catch (error) {
       console.error(error.message);
     }
@@ -102,42 +107,42 @@ const Signup = () => {
     setShowLoginForm(!showLoginForm);
   };
 
-  
+
   return (
     <Align>
-    <div className="grid align__item">
-      <RegisterWrapper>
+      <div className="grid align__item">
+        <RegisterWrapper>
 
-        <svg xmlns="http://www.w3.org/2000/svg" className="site__logo" width="56" height="84" viewBox="77.7 214.9 274.7 412"><defs><linearGradient id="a" x1="0%" y1="0%" y2="0%"><stop offset="0%" stopColor="#8ceabb"/><stop offset="100%" stopColor="#378f7b"/></linearGradient></defs><path fill="url(#a)" d="M215 214.9c-83.6 123.5-137.3 200.8-137.3 275.9 0 75.2 61.4 136.1 137.3 136.1s137.3-60.9 137.3-136.1c0-75.1-53.7-152.4-137.3-275.9z"/></svg>
+          <img src="https://firebasestorage.googleapis.com/v0/b/quickbite-844b3.appspot.com/o/logo_quickbite.png?alt=media&token=356b8ce3-e2e0-4584-a46a-656992a181f3" className="site__logo" width="56" height="84" viewBox="77.7 214.9 274.7 412"><defs><linearGradient id="a" x1="0%" y1="0%" y2="0%"><stop offset="0%" stopColor="#8ceabb" /><stop offset="100%" stopColor="#378f7b" /></linearGradient></defs></img>
 
-        <h2>Sign Up</h2>
+          <h2>Sign Up</h2>
 
-        <form action="" method="post" className="form">
+          <form action="" method="post" className="form">
 
-          <FormField>
-            <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="email" required />
-          </FormField>
+            <FormField>
+              <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="email" required />
+            </FormField>
 
-          <FormField>
-            <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="parola" required />
-          </FormField>
+            <FormField>
+              <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="parola" required />
+            </FormField>
 
-          <FormField>
-            <Input type="password" value={conf_pass} onChange={(e) => setConf_pass(e.target.value)} placeholder="confirmare parola" required />
-          </FormField>
+            <FormField>
+              <Input type="password" value={conf_pass} onChange={(e) => setConf_pass(e.target.value)} placeholder="confirmare parola" required />
+            </FormField>
 
-          <FormField>
-            <SubmitButton type="submit" onClick={handleSignup} value="Sign Up" />
-            <SubmitButton type="submit" onClick={handleGoogleSignup} value="Sign Up with Google" />
-          </FormField>
+            <FormField>
+              <SubmitButton type="submit" onClick={handleSignup} value="Sign Up" />
+              <SubmitButton type="submit" onClick={handleGoogleSignup} value="Sign Up with Google" />
+            </FormField>
 
-        </form>
+          </form>
 
-        <p>Already have an account? <a href="#">Log in</a></p>
+          <p>Already have an account? <a href="#">Log in</a></p>
 
-      </RegisterWrapper>
-    </div>
-  </Align>
+        </RegisterWrapper>
+      </div>
+    </Align>
   );
 };
 
