9ef5d2c847a826c6614bd7624ca70b89cbfa2a01
Matei-Alexandru Ciucă <86956363+matei1903@users.noreply.github.com>
2024-05-11 02:23:38 +0300

    login page

--- apps/client/pages/Login.jsx
+++ apps/client/pages/Login.jsx
@@ -1,30 +1,129 @@
 import React, { useState } from 'react';
-import { getAuth } from "firebase/auth";
+import { getAuth, handleLogin } from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import styled from 'styled-components';
+
+const Align = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  background-image: url("https://firebasestorage.googleapis.com/v0/b/quickbite-844b3.appspot.com/o/360_F_637166455_RZOHg6K3M6noLPKQmOHH5ZI70zXEyDaq.jpg?alt=media&token=af02128c-0b80-4fdc-af73-ae565a3c3cbd");
+  background-color: rgba(255, 255, 255, 0.5); /* Opacitatea de 0.3 (70%) */
+  background-blend-mode: lighten;
+  background-size: cover;
+`;
+
+const RegisterWrapper = styled.div`
+  text-align: center;
+  padding: 80px;
+  padding-top: 30px;
+  width: 60%;
+  margin: 0 auto;
+  background-color: #202b1b;
+`;
+
+const FormField = styled.div`
+  margin-bottom: 1rem;
+  border-radius: 25px;
+  text-align: center;
+  justify-content: center;
+`;
+
+const Input = styled.input`
+  border: 0;
+  font: inherit;
+  padding: 0.5rem;
+  width: 90%;
+  border-radius: 25px;
+  text-align: center;
+  justify-content: center;
+
+  &::placeholder {
+    color: #7e8ba3;
+    text-align: center;
+  }
+
+  &[type="email"],
+  &[type="password"] {
+   
+    outline: 0;
+    
+  }
+`;
+
+const Img = styled.img`
+  margin-top: 20px;
+  width: 250px;
+  height: auto;
+  margin-left: auto;
+  mergin-right: auto;
+  }
+  `;
+
+const SubmitButton = styled.input`
+  background: linear-gradient(160deg, #8ceabb 0%, #378f7b 100%);
+  border: 0;
+  border-radius: 999px; /* Am eliminat spațiul în jurul operatorului - */
+  color: #fff;
+  margin-bottom: 10px;
+  margin-top: 30px; /* Am eliminat spațiul în jurul operatorului - */
+  width: 100%;
+  font-size: 20px; /* Am eliminat spațiul în jurul operatorului - */
+  padding: 5px 5px; /* Am eliminat spațiul în jurul operatorului - */
+`;
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const auth = getAuth();
+ // const auth = getAuth();
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      await auth.signInWithEmailAndPassword(email, password);
+      const user = await handleLogin(email, password);
+      navigate('/home');
       // Redirect user to dashboard or any other page after login
     } catch (error) {
-      console.error(error.message);
+      alert("Emailul sau parola sunt incorecte");
     }
   };
 
+const navigateToSignup = () => {
+    navigate('/Signup');
+  };
+
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <Align>
+      <div className="grid align__item">
+        <RegisterWrapper>
+          <Img src="https://firebasestorage.googleapis.com/v0/b/quickbite-844b3.appspot.com/o/logo_quickbite.png?alt=media&token=356b8ce3-e2e0-4584-a46a-656992a181f3" className="site_logo"></Img>
+
+          <h2>Sign Up</h2>
+
+          <form action="" method="post" className="form">
+
+            <FormField>
+              <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="email" required />
+            </FormField>
+
+            <FormField>
+              <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="parola" required />
+            </FormField>
+
+            <FormField>
+              <SubmitButton type="submit" onClick={handleLogin} value="Sign In" />
+            </FormField>
+
+          </form>
+
+          <p>You don't have an account? <a href="#" onClick={navigateToSignup}>Create an account</a></p>
+
+        </RegisterWrapper>
+      </div>
+    </Align>
   );
 };
 
