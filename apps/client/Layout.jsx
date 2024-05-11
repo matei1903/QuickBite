406aad26e800adb2e0ac7a99f7dab276dac08cae
Matei-Alexandru Ciucă <86956363+matei1903@users.noreply.github.com>
2024-05-11 13:59:38 +0300

    fix17_styling

--- apps/client/Layout.jsx
+++ apps/client/Layout.jsx
@@ -42,7 +42,7 @@ const Nav = styled.nav`
   border: 1px solid #ccc;
   border-radius: 5px;
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-  display: ${({ isMenuOpen }) => (isMenuOpen ? 'block' : 'none')};
+  display: ${({ isMenuOpen }) => (isMenuOpen ? 'flex' : 'none')};
   z-index: 999; /* Ensure menu is above other content */
   justify-content: right;
 }
