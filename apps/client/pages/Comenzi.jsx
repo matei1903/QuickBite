4f96f75fa8a9ebddc82d22163501844f166f0d9a
Matei-Alexandru Ciucă <86956363+matei1903@users.noreply.github.com>
2024-05-14 18:20:03 +0300

    styling5_pagina_comenzi

--- apps/client/pages/Comenzi.jsx
+++ apps/client/pages/Comenzi.jsx
@@ -8,23 +8,16 @@ const Coloana = styled.div`
     padding: 10px;
 `;
 
-const ColoanaStanga = styled(Coloana)`
-    border-right: 2px solid black;
-`;
-
-const ColoanaDreapta = styled(Coloana)`
-    border-left: 2px solid black;
-`;
 
 const comenzi = () => {
     return (
         <Layout>
-            <ColoanaStanga>
+            <Coloana>
                 <h2>Comenzile mele</h2>
-            </ColoanaStanga>
-            <ColoanaDreapta>
+            </Coloana>
+            <Coloana>
                 <h2>Comenzile mesei</h2>
-            </ColoanaDreapta>
+            </Coloana>
         </Layout>
     );
 };
