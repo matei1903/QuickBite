7339c25e87f8c93223c0b16ca96d363c5539ba9f
Matei-Alexandru Ciucă <86956363+matei1903@users.noreply.github.com>
2024-05-14 18:16:20 +0300

    styling4_pagina_comenzi

--- apps/client/pages/Comenzi.jsx
+++ apps/client/pages/Comenzi.jsx
@@ -3,34 +3,29 @@ import styled from "styled-components";
 const Layout = React.lazy(() => import("../Layout.jsx"));
 
 const Coloana = styled.div`
-    
-    .column_left {
-        border-right: 2px solid black;
-        float: left;
-        width: 50%;
-        padding: 10px;
-    }
-    .column_right {
-        border-left: 2px solid black;
-        float: left;
-        width: 50%;
-        padding: 10px;
-    }
-  
+    float: left;
+    width: 50%;
+    padding: 10px;
+`;
+
+const ColoanaStanga = styled(Coloana)`
+    border-right: 2px solid black;
+`;
+
+const ColoanaDreapta = styled(Coloana)`
+    border-left: 2px solid black;
 `;
 
 const comenzi = () => {
     return (
-        <div>
-            <Coloana className="column_left">
+        <Layout>
+            <ColoanaStanga>
                 <h2>Comenzile mele</h2>
-            </Coloana>
-            <Coloana className="column_right">
+            </ColoanaStanga>
+            <ColoanaDreapta>
                 <h2>Comenzile mesei</h2>
-            </Coloana>
-        </div>
-
-
+            </ColoanaDreapta>
+        </Layout>
     );
 };
 export default comenzi;
