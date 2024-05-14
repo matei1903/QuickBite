da1ce5e07b2b56434e233f32bf479e1a60771743
Matei-Alexandru Ciucă <86956363+matei1903@users.noreply.github.com>
2024-05-14 19:09:18 +0300

    fix_padding

--- apps/client/pages/Comenzi.jsx
+++ apps/client/pages/Comenzi.jsx
@@ -5,24 +5,25 @@ const Layout = React.lazy(() => import("../Layout.jsx"));
 const ColoanaS = styled.div`
     float: left;
     width: 45%;
-    padding: 12px;
+    padding: 10px;
     border-right: 1px solid black;
     
     h2 {
         text-align: center;
-        border-bottom: 1px solid black;
+        border-bottom: 2px solid black;
+        
     }
     
 `;  
 const ColoanaD = styled.div`
     float: right;
     width: 45%;
-    padding: 12px;
+    padding: 10px;
     border-left: 1px solid black;
     
     h2 {
         text-align: center;
-        border-bottom: 1px solid black;
+        border-bottom: 2px solid black;
     }
     
 `;  
