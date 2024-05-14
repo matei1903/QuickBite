feff645525e56d9b0ca014e755e29500a3da22e6
Matei-Alexandru Ciucă <86956363+matei1903@users.noreply.github.com>
2024-05-14 18:35:42 +0300

    styling6_pagina_comenzi

--- apps/client/pages/Comenzi.jsx
+++ apps/client/pages/Comenzi.jsx
@@ -4,9 +4,19 @@ const Layout = React.lazy(() => import("../Layout.jsx"));
 
 const Coloana = styled.div`
     float: left;
-    width: 50%;
+    width: 48%;
     padding: 10px;
-`;
+    
+    h2 {
+        justify-content: center;
+    }
+    .vl {
+        float: left;
+        border-left: 6px solid black;
+        height: 100%;
+      }
+    
+`;  
 
 
 const comenzi = () => {
@@ -15,6 +25,7 @@ const comenzi = () => {
             <Coloana>
                 <h2>Comenzile mele</h2>
             </Coloana>
+            <div class="vl"></div>
             <Coloana>
                 <h2>Comenzile mesei</h2>
             </Coloana>
