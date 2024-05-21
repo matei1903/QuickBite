f7da325aa7513d26c38b75b1415e6dde29c36dec
Matei-Alexandru Ciucă <86956363+matei1903@users.noreply.github.com>
2024-05-22 00:00:09 +0300

    styling2_popup_custom

--- apps/client/pages/CustomPlata.jsx
+++ apps/client/pages/CustomPlata.jsx
@@ -8,8 +8,8 @@ const PopupContainer = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
+  width: 80%;
+  height: 200px;
   background: rgba(0, 0, 0, 0.5);
   display: flex;
   justify-content: center;
@@ -26,7 +26,6 @@ const PopupContent = styled.div`
   border-radius: 10px;
   width: 400px;
   max-width: 100%;
-  overflow: auto;
   height: 200px;
 `;
 
